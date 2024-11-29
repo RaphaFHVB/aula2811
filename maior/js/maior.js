9012e11df77615291d0625eedd3b7c670fa66080
RaphaFHVB <raphael.nascimento36@etec.sp.gov.br>
2024-11-28 21:29:49 -0300

    aicionando arquivo maior

---
+++ maior/js/maior.js
@@ -0,0 +1,34 @@
+var numero1 = parseFloat(prompt("Digite o primeiro numero: "))
+var numero2 = parseFloat(prompt("Digite o segundo numero: "))
+
+
+if (numero1 > numero2){
+
+alert("o maior numero é o primeiro: "+ numero1)
+
+}else{
+
+    alert("o maior numero é o segundo: "+ numero2)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
